2ff573c418d02ba478be770be8b95311364ed690
masozow <14838605+masozow@users.noreply.github.com>
2025-06-09 17:03:53 -0600

    solving some validation errors in registerForm

--- frontend-lifestyle-plan/src/schemas/registerFormSchema.ts
+++ frontend-lifestyle-plan/src/schemas/registerFormSchema.ts
@@ -5,12 +5,12 @@ export const schema_registerForm = z.object({
   name: z.string().min(2),
   email: z.string().email(),
   password: z.string().min(6),
-  phone: z.string().min(8),
+  phone: z.string().min(8).regex(/^\d+$/, { message: "phone_invalid_format" }), // Use a translation key
   birthDate: z.string().refine((value) => {
     const age = diffYears(new Date(), new Date(value));
     return age >= 18;
   }, {
-    message: "Must be at least 18 years old",
+    message: "birthdate_min_age",
   }),
   gender: z.enum(["male", "female"]),
   statusId: z.number(),
@@ -18,4 +18,3 @@ export const schema_registerForm = z.object({
 });
 
 export type RegisterFormValues = z.infer<typeof schema_registerForm>;
-
