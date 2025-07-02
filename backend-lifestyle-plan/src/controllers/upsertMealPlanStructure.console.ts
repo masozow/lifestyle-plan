6dd75d4c0615586303dfcedf0b231fc22b9b2fde
masozow <14838605+masozow@users.noreply.github.com>
2025-07-02 08:44:52 -0600

    commenting the console file used to fine tune the upsertMealPlanStructure

--- backend-lifestyle-plan/src/controllers/upsertMealPlanStructure.console.ts
+++ backend-lifestyle-plan/src/controllers/upsertMealPlanStructure.console.ts
@@ -1,22 +1,22 @@
-import { upsertMealPlanStructure } from './upsertMealPlanStructure.ts';
-import mockData from '../db/mockDataToShapeFrontend.json' with { type: 'json' };
+// import { upsertMealPlanStructure } from './upsertMealPlanStructure.ts';
+// import mockData from '../db/mockDataToShapeFrontend.json' with { type: 'json' };
 
-async function main() {
-  const userId = 1;
-  const openAIResponseId = 1;
-  const weeklyPlan = mockData.weekly_plan;
-  const transaction = null; // We don't need a transaction for this test
+// async function main() {
+//   const userId = 1;
+//   const openAIResponseId = 1;
+//   const weeklyPlan = mockData.weekly_plan;
+//   const transaction = null; // We don't need a transaction for this test
 
-  const result = await upsertMealPlanStructure({
-    userId,
-    userMealProgressId: 1,
-    openAIResponseId,
-    weeklyPlan,
-    transaction,
-  });
+//   const result = await upsertMealPlanStructure({
+//     userId,
+//     userMealProgressId: 1,
+//     openAIResponseId,
+//     weeklyPlan,
+//     transaction,
+//   });
 
-//   console.log('Result:');
-//   console.log(result);
-}
+// //   console.log('Result:');
+// //   console.log(result);
+// }
 
-main();
+// main();
