4226c331b5f831ada64f6acd235009eca256629d
masozow <14838605+masozow@users.noreply.github.com>
2025-07-03 11:25:45 -0600

    solving the order of dates in dashboardhome and removing gradient from cards

--- frontend-lifestyle-plan/src/components/section-cards.tsx
+++ frontend-lifestyle-plan/src/components/section-cards.tsx
@@ -9,7 +9,7 @@ export function SectionCards() {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5, ease: "easeInOut" }}
-      className="*:data-[slot=card]:from-primary/4 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-3"
+      className="dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-3"
     >
       <ObjectivesCard className="@container/card" />
       <ProfileCard className="@container/card" />
