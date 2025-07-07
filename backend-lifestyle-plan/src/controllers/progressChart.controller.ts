5ce51cf590eefc0341d72b7930603df0a74d71d7
masozow <14838605+masozow@users.noreply.github.com>
2025-07-07 11:56:13 -0600

    adding a stored procedure instead of a parametrized query

--- backend-lifestyle-plan/src/controllers/progressChart.controller.ts
+++ backend-lifestyle-plan/src/controllers/progressChart.controller.ts
@@ -28,73 +28,83 @@ const getUserProgressData = async (req: Request, res: Response) => {
   }
 
   try {
-       const results = await sequelize.query<ProgressRow>(
-      `
-        SELECT
-          T.date,
-          T.targetEnergy,
-          COALESCE(C.consumedEnergy, 0) AS consumedEnergy,
-          T.targetProtein,
-          COALESCE(C.consumedProtein, 0) AS consumedProtein,
-          T.targetCarbs,
-          COALESCE(C.consumedCarbs, 0) AS consumedCarbs,
-          T.targetFat,
-          COALESCE(C.consumedFat, 0) AS consumedFat
-        FROM (
-          SELECT
-            M.date,
-            SUM(M.targetEnergy) AS targetEnergy,
-            SUM(M.targetProtein) AS targetProtein,
-            SUM(M.targetCarbs) AS targetCarbs,
-            SUM(M.targetFat) AS targetFat
-          FROM nutrition.userDailyMeal AS M
-          JOIN nutrition.userMealProgress AS U
-            ON M.userMealProgressId = U.id
-          JOIN (
-            SELECT 
-              M2.date,
-              MAX(M2.userMealProgressId) AS MaxProgressId
-            FROM nutrition.userDailyMeal AS M2
-            JOIN nutrition.userMealProgress AS U2
-              ON M2.userMealProgressId = U2.id
-            WHERE U2.userId = :userId
-            GROUP BY M2.date
-          ) AS latest
-            ON M.date = latest.date
-          AND M.userMealProgressId = latest.MaxProgressId
-          WHERE U.userId = :userId
-          GROUP BY M.date
-        ) AS T
-        LEFT JOIN (
-          SELECT
-            M.date,
-            SUM(COALESCE(I.consumedEnergy, M.targetEnergy)) AS consumedEnergy,
-            SUM(COALESCE(I.consumedProtein, M.targetProtein)) AS consumedProtein,
-            SUM(COALESCE(I.consumedCarbs, M.targetCarbs)) AS consumedCarbs,
-            SUM(COALESCE(I.consumedFat, M.targetFat)) AS consumedFat
-          FROM nutrition.userDailyMeal AS M
-          JOIN nutrition.userMealProgress AS U
-            ON M.userMealProgressId = U.id
-          LEFT JOIN nutrition.userDailyIntake AS I
-            ON I.userDailyMealId = M.id
-          WHERE U.userId = :userId
-            AND (I.consumed = 1 OR M.consumed = 1)
-          GROUP BY M.date
-        ) AS C
-        ON T.date = C.date
-        ORDER BY T.date;
-      `,{
-        replacements: { userId },
-        type: QueryTypes.SELECT,
+    //    const results = await sequelize.query<ProgressRow>(
+    //   `
+    //     SELECT
+    //       T.date,
+    //       T.targetEnergy,
+    //       COALESCE(C.consumedEnergy, 0) AS consumedEnergy,
+    //       T.targetProtein,
+    //       COALESCE(C.consumedProtein, 0) AS consumedProtein,
+    //       T.targetCarbs,
+    //       COALESCE(C.consumedCarbs, 0) AS consumedCarbs,
+    //       T.targetFat,
+    //       COALESCE(C.consumedFat, 0) AS consumedFat
+    //     FROM (
+    //       SELECT
+    //         M.date,
+    //         SUM(M.targetEnergy) AS targetEnergy,
+    //         SUM(M.targetProtein) AS targetProtein,
+    //         SUM(M.targetCarbs) AS targetCarbs,
+    //         SUM(M.targetFat) AS targetFat
+    //       FROM nutrition.userDailyMeal AS M
+    //       JOIN nutrition.userMealProgress AS U
+    //         ON M.userMealProgressId = U.id
+    //       JOIN (
+    //         SELECT 
+    //           M2.date,
+    //           MAX(M2.userMealProgressId) AS MaxProgressId
+    //         FROM nutrition.userDailyMeal AS M2
+    //         JOIN nutrition.userMealProgress AS U2
+    //           ON M2.userMealProgressId = U2.id
+    //         WHERE U2.userId = :userId
+    //         GROUP BY M2.date
+    //       ) AS latest
+    //         ON M.date = latest.date
+    //       AND M.userMealProgressId = latest.MaxProgressId
+    //       WHERE U.userId = :userId
+    //       GROUP BY M.date
+    //     ) AS T
+    //     LEFT JOIN (
+    //       SELECT
+    //         M.date,
+    //         SUM(COALESCE(I.consumedEnergy, M.targetEnergy)) AS consumedEnergy,
+    //         SUM(COALESCE(I.consumedProtein, M.targetProtein)) AS consumedProtein,
+    //         SUM(COALESCE(I.consumedCarbs, M.targetCarbs)) AS consumedCarbs,
+    //         SUM(COALESCE(I.consumedFat, M.targetFat)) AS consumedFat
+    //       FROM nutrition.userDailyMeal AS M
+    //       JOIN nutrition.userMealProgress AS U
+    //         ON M.userMealProgressId = U.id
+    //       LEFT JOIN nutrition.userDailyIntake AS I
+    //         ON I.userDailyMealId = M.id
+    //       WHERE U.userId = :userId
+    //         AND (I.consumed = 1 OR M.consumed = 1)
+    //       GROUP BY M.date
+    //     ) AS C
+    //     ON T.date = C.date
+    //     ORDER BY T.date;
+    //   `,{
+    //     replacements: { userId },
+    //     type: QueryTypes.SELECT,
+    //   }
+    // );
+
+    const results = await sequelize.query(
+      "CALL sp_getUserProgressData(:userId)",
+      {
+        replacements: { userId }
       }
     );
+    
+    const rows = results as unknown as ProgressRow[];
     const data = {
       energy: [],
       protein: [],
       carbs: [],
       fat: [],
     };
-    results.forEach((row) => {
+    //with parametrized query, results should be used instead of "rows"
+    rows.forEach((row) => {
       data.energy.push({
         date: row.date,
         target: row.targetEnergy,
