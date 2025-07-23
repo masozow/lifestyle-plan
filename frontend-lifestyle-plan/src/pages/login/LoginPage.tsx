ba4bcb747e221c4d55160d43e7c3156eac8ba894
masozow <14838605+masozow@users.noreply.github.com>
2025-07-23 10:19:56 -0600

    adding translations to landingpage and finishing the checkTranslation function for visualizers, and making it reusable in any part of the code

--- frontend-lifestyle-plan/src/pages/login/LoginPage.tsx
+++ frontend-lifestyle-plan/src/pages/login/LoginPage.tsx
@@ -1,12 +1,14 @@
 import { LoginForm } from "@/components";
 import landing_image from "../landing/landing_image.png";
 import { AuthFormWrapper } from "@/layouts";
+import { useTranslation } from "react-i18next";
 
 export const LoginPage = () => {
+  const { t } = useTranslation();
   return (
     <AuthFormWrapper
       landing_image_url={landing_image}
-      landing_image_alt="Woman cooking"
+      landing_image_alt={t("loginPage.imageAlt")}
     >
       <LoginForm />
     </AuthFormWrapper>
